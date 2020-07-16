983922a2a15e2d91e377a34f7c6d95e1662db90e
rgbkad <kaderarnold@icloud.com>
2020-07-16 13:50:10 -0400

    changed the <form> element so that it no longer has a blank action='' attribute, now it only includes the method='post' argument. i also deleted a swp file so I didnt get confused behind the scenes. also added the port to our php connector just to be sure that during development we are using our xampp instance of mysql instead of any lingering unwanted downloads from previous brew installs

--- submit.php
+++ submit.php
@@ -1,12 +1,12 @@
 <?php
 
-	$username = 'root';
+	$username = 'kader';
 	$password = 'kader';
-	$hostname = 'localhost';
+	$hostname = 'kaders-macbook-air.local';
 	$port = 3306;
 	$dbname = 'db';
 
-	$link = new mysqli($username,$password,$hostname,$dbname);
+	$link = new mysqli($username,$password,$hostname,$dbname,$port);
 	$sql = "insert into customer
 		values (".$_POST['fullname'].",".$_POST['email'].");";
 
